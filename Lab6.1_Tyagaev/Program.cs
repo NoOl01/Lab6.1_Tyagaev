b109dd4ef40617b6d17929150aa9b65b8627a64f
Anton <Anton@DESKTOP-6RE5PBQ>
2023-12-28 03:35:22 +0200

    Добавьте файлы проекта.

---
+++ Lab6.1_Tyagaev/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Random random = new Random();
+int[,] matrix = new int[n, m];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = random.Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+double result = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        result += Math.Pow(matrix[i, j], 2) * Math.Pow(2, i + j);
+    }
+}
+
+result = Math.Sqrt(result);
+Console.WriteLine($"Результат: {result:F2}");
